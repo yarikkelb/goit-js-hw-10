c94388e31bcaaa107e289533d6042c1bda718bf7
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-07-16 11:02:38 +0300

    1103

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ function onFetchError(error) {
     loader.classList.replace('loader', 'is-hidden');
 
     Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
-        position: 'center-center',
+        position: 'center',
         timeout: 5000,
         width: '400px',
         fontSize: '24px'
