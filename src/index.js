4d15b4d1918dc34bfcb52f56eb47ffec1bb361e7
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-07-18 18:41:18 +0300

    1841

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const { selector, divCatInfo, loader, error } = ref;
 
 loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
-divCatInfo.classList.add();
+divCatInfo.classList.add('is-hidden');
 
 let arrBreedsId = [];
 fetchBreeds()
@@ -34,7 +34,7 @@ selector.addEventListener('change', onSelectBreed);
 function onSelectBreed(event) {
     loader.classList.replace('is-hidden', 'loader');
     selector.classList.add('is-hidden');
-    divCatInfo.classList.add('is-hidden');
+    //divCatInfo.classList.add('is-hidden');
 
     const breedId = event.currentTarget.value;
     fetchCatByBreed(breedId)
@@ -44,7 +44,7 @@ function onSelectBreed(event) {
         const { url, breeds } = data[0];
         
         divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
-        divCatInfo.classList.remove('is-hidden');
+        divCatInfo.classList.remove ;
     })
     .catch(onFetchError);
 };
